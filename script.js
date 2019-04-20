a82d860814875978609965067e8db2c0627efd36
Godsgift Akari <giftakari@yahoo.com>
2019-04-21 06:12:41 +1000

    Complete Project

---
+++ script.js
@@ -0,0 +1,78 @@
+const createPolicy =(name, amount)=>{
+  return{
+    type: 'CREATE_POLICY',
+    payload:{
+      name: name,
+      amount:amount
+    }
+  };
+};
+
+const deletePolicy =(name) =>{
+  return{
+    type: 'DELETE_POLICY',
+    payload: {
+      name:name
+    }
+  };
+};
+
+const createClaim =(name, amountOfMoneyToCollect) =>{
+  return {
+    type: 'CREATE_CLAIM',
+    payload: {
+      name: name,
+      amountOfMoneyToCollect:amountOfMoneyToCollect
+    }
+  }
+}
+//Reducers (Departments)
+const claimsHistory = (oldListOfClaims=[],action) => {
+if (action.type === 'CREATE_CLIAM'){
+  //We care about the form
+  return [...oldListOfClaims, action.payload]
+}
+ return oldListOfClaims;
+}
+
+const accounting = (bagOfMoney= 100, action) => {
+  if(action.type === 'CREATE_CLAIM' ){
+  return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } 
+  else if (action.type === 'CREATE_POLICY'){
+    return bagOfMoney + action.payload.amount
+  }
+  return bagOfMoney;
+}
+
+const policies =  (listOfPolicies = [], action) =>{
+if (action.type ==='CREATE_POLICY'){
+  [...listOfPolicies, action.payload.name]; 
+}
+else if ( action.type ==='DELETE_POLICY'){
+  return listOfPolicies.filter(name => name !== action.payload.name);
+}
+return listOfPolicies;
+}
+
+
+const {createStore, combineReducers} =Redux;
+
+const ourDepartments = combineReducers({
+  accounting,
+  claimsHistory,
+  policies,
+})
+
+const store = createStore(ourDepartments);
+
+
+
+store.dispatch(createPolicy('Alex', -10));
+store.dispatch(createPolicy('Jim', 50))
+store.dispatch(createPolicy('Gift', 30))
+//make claims
+store.dispatch(createClaim('Gift', 30))
+
+console.log(store.getState()); 
+
